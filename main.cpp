02d55193e1b2411eba9e804e77890619a7968de8
Azimjon Pulatov <gmail@azimjon.com>
2021-04-11 11:56:57 +0200

    Add Game Driver Code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,69 @@
 #include <iostream>
+#include "board.h"
+#include "gameover.h"
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+void start_game(board_t* board);
+
+void show_manual()
+{
+    cout << "Game Manual: " << endl;
+    cout << "\tS - screenshot" << endl;
+    cout << "\tR {r} {c} - reveal {r} {c} cell" << endl;
+    cout << "\tF {r} {c} - flag   {r} {c} cell" << endl;
+    cout << "\tU {r} {c} - unflag {r} {c} cell" << endl;
+}
 
 int main(){
-    cout << "Hello World";
+    int width = 20, height = 10, mines = 42;
+
+    board_t board{width, height};
+    board.place_mines(mines);
+    show_manual();
+
+    try {
+       start_game(board);
+    } catch(GameOver e){
+        board.reveal_all_cells();
+        board.display();
+        cout << "Game Over :(";
+    }
+}
+
+void start_game(board_t* board){
+    char action;
+    int row, col;
+    board->display();
+
+    while(true)
+    {
+        cout << "Enter Action: ";
+        cin >> action;
+
+        switch (action)
+        {
+        case 'S', 's':
+            board->screenshot();
+            break;
+        case 'R', 'r':
+            cin >> row >> col;
+            board->reveal_cell(row, col);
+            break;
+        case 'F', 'f':
+            cin >> row >> col;
+            board->flag_cell(row, col);
+            break;
+        case 'U', 'u':
+            cin >> row >> col;
+            board->unflag_cell(row, col);
+            break;
+        default:
+            cout << "Unknown Action %c" % action << endl;
+            break;
+        }
+        board->display();
+    }
 }
