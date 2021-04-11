32f53d60dc71a042b5b8a2b8c43e66401bd9f920
Azimjon Pulatov <gmail@azimjon.com>
2021-04-11 11:26:56 +0200

    Update board struct

--- board.h
+++ board.h
@@ -1,14 +1,23 @@
+#include <vector>
 #include "cell.h"
 
+using std::vector;
 
 struct board_t
 {
-    cell_t** cells = nullptr;
+private:
     int width;
     int height;
+    vector<vector<cell_t*>> cells;
 
+public:
     board_t(const int W, const int H);
     void place_mines(const int count);
     void display();
+    void screenshot();
     void reveal_cell(const int row, const int col);
+    void reveal_all_cells();
+    void flag_cell(const int row, const int col);
+    void unflag_cell(const int row, const int col);
+    ~board_t();
 };
