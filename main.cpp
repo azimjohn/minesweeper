c9835cc526f99d3f638b98a8eaadc8d11b15b925
Azimjon Pulatov <gmail@azimjon.com>
2021-06-06 11:43:08 +0200

    add game controls in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-
+#include "texture_manager.h"
 #include "board.h"
 #include "gameover.h"
 
-#define BOARD_WIDTH 15
-#define BOARD_HEIGHT 15
+#define BOARD_WIDTH 12
+#define BOARD_HEIGHT 12
 #define MINES 20
 #define CELL_SIZE 50
+#define KEY_R 17
 
 using std::cin;
 using std::cout;
@@ -18,25 +19,60 @@ int main() {
     board_t board(BOARD_WIDTH, BOARD_HEIGHT);
     board.place_mines(MINES);
 
+    bool isGameOver = false;
     int width = BOARD_WIDTH * CELL_SIZE;
     int height = BOARD_HEIGHT * CELL_SIZE;
     auto style = sf::Style::Titlebar | sf::Style::Close;
-    sf::RenderWindow window(sf::VideoMode(width, height), "Minesweeper Game", style);
+    sf::RenderWindow window(sf::VideoMode(width, height), "MineSweeper Game", style);
+    TextureManager textureManager;
+    textureManager.loadAll();
+
+    board.draw(&window, &textureManager, CELL_SIZE);
+    window.display();
 
     while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+                break;
+            }
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == KEY_R){
+                    isGameOver = false;
+                    board.reset();
+                    board.place_mines(MINES);
+                }
+                cout << event.key.code;
+            }
+            else if (event.type == sf::Event::MouseButtonPressed && !isGameOver)
+            {
+                int i = event.mouseButton.x / CELL_SIZE;
+                int j = event.mouseButton.y / CELL_SIZE;
+
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    try
+                    {
+                        board.reveal_cell(i, j);
+                    }
+                    catch(GameOver& e)
+                    {
+                        isGameOver = true;
+                        board.reveal_all_cells();
+                    }
+                }
+                else
+                {
+                    board.flag_cell(i, j);
+                }
+            }
+
+            board.draw(&window, &textureManager, CELL_SIZE);
+            window.display();
         }
-
-        window.clear(sf::Color::Black);
-
-        // draw everything here...
-        // window.draw(...);
-
-        window.display();
     }
 }
